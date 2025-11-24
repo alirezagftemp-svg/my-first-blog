5b3b38689e787b3e8a894a48fe7c8baa1e28f42e
alireza <alireza.gf.temp@gmail.com>
2025-11-24 22:50:40 +0330

    phase 5

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Post
 from django.utils import  timezone
 from django.shortcuts import render, get_object_or_404
-
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(
@@ -15,4 +15,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
- 
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
